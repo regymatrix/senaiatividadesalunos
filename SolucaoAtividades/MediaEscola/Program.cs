76441ad22656e5c00836d6b109ed376d78e9c31a
regymatrix <105677816+regymatrix@users.noreply.github.com>
2025-11-24 15:59:33 -0300

    nota 4 adicionada

--- SolucaoAtividades/MediaEscola/Program.cs
+++ SolucaoAtividades/MediaEscola/Program.cs
@@ -5,8 +5,10 @@ Console.WriteLine("Digite a segunda nota");
 decimal nota2 = Convert.ToDecimal(Console.ReadLine());
 Console.WriteLine("Digite a terceira nota");
 decimal nota3 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite a terceira nota");
+decimal nota4 = Convert.ToDecimal(Console.ReadLine());
 
-decimal media = (nota1 + nota2 + nota3) / 3;
+decimal media = (nota1 + nota2 + nota3+nota4) / 3;
 
 Console.WriteLine($"A média é: {media} ");
 

c461f0cf7b48f1f3a7161f3dc167b67368941794
regymatrix <105677816+regymatrix@users.noreply.github.com>
2025-11-24 15:39:12 -0300

    projeto inicio

---
+++ SolucaoAtividades/MediaEscola/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("PROGAMA CALCULAR MÉDIA");
+Console.WriteLine("Digite a primeira nota");
+decimal nota1 =Convert.ToDecimal ( Console.ReadLine());
+Console.WriteLine("Digite a segunda nota");
+decimal nota2 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite a terceira nota");
+decimal nota3 = Convert.ToDecimal(Console.ReadLine());
+
+decimal media = (nota1 + nota2 + nota3) / 3;
+
+Console.WriteLine($"A média é: {media} ");
+
+if (media >= 7)
+{
+    Console.WriteLine("Aprovado");
+}
+else
+{
+    Console.WriteLine("Reprovado");
+}

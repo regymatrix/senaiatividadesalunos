28d402596deb45e090a70c79508823e881b9b6ba
regymatrix <105677816+regymatrix@users.noreply.github.com>
2025-11-26 16:18:32 -0300

    imc

---
+++ projetoimc/projetoimc/Program.cs
@@ -0,0 +1,29 @@
+﻿double pesoEmKg;
+double alturaEmMetros;
+double imc;
+
+Console.WriteLine("Digite o peso: ");
+pesoEmKg = Convert.ToDouble( Console.ReadLine());
+Console.WriteLine("Digite a aultra: ");
+alturaEmMetros = Convert.ToDouble(Console.ReadLine());
+
+if (alturaEmMetros == 0)
+{
+    Console.WriteLine("Altura deve ser maior que zero");
+    return;
+}
+
+double alturaAoQuadrado = alturaEmMetros * alturaEmMetros;
+imc = pesoEmKg / alturaAoQuadrado;
+double imcFormatado = Math.Round(imc, 2);
+
+Console.WriteLine($"O Imc é: {imcFormatado}");
+
+if (imcFormatado <= 29.9)
+{
+    Console.WriteLine("Margro");
+}
+else
+{
+    Console.WriteLine("Obesidade");
+}
